5627031de1d4ae3344d965179ede39578da34134
romain-abc <romain@abc-com.fr>
2024-01-17 18:10:44 +0100

    bugfixes list reliquats

--- htdocs/commande/list_reliquats.php
+++ htdocs/commande/list_reliquats.php
@@ -216,6 +216,8 @@ $parameters = array();
 $reshook = $hookmanager->executeHooks('printFieldListSelect', $parameters, $object, $action); // Note that $action and $object may have been modified by hook
 
 $sqlquery0 = "SELECT rowid FROM ".MAIN_DB_PREFIX."propal as p";
+$sqlquery0 .= " WHERE p.fk_statut = ".Propal::STATUS_VALIDATED;
+
 $ressqlquery0 = $db->query($sqlquery0);
 foreach($ressqlquery0 as $q0) {
 	$sqlquery = "SELECT rowid, fk_product, rang, qty, fk_propal FROM " . MAIN_DB_PREFIX . "propaldet as pdt";
