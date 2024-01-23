f39e1340f77cdb406f7c98103d53995cc2e2ed7a
romain-abc <romain@abc-com.fr>
2024-01-23 17:21:39 +0100

    Update card.php

--- htdocs/compta/facture/card.php
+++ htdocs/compta/facture/card.php
@@ -5532,7 +5532,7 @@ if ($action == 'create') {
 
 	// Show object lines
 	if (!empty($object->lines)) {
-		$object->printObjectLines($action, $mysoc, $soc, $lineid, 1);
+		$object->printObjectLinesPropal($action, $mysoc, $soc, $lineid, 1);
 	}
 
 	// Form to add new line
