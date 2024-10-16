4e8ed0938e5b96afb3ae9b40902a64588c888198
romain-abc <romain@abc-com.fr>
2024-10-16 11:24:26 +0200

    Update objectline_create_propal.tpl.php

--- htdocs/core/tpl/objectline_create_propal.tpl.php
+++ htdocs/core/tpl/objectline_create_propal.tpl.php
@@ -123,9 +123,9 @@ if ($nolinesbefore) {
 		<?php if (isModEnabled("multicurrency") && $this->multicurrency_code != $conf->currency) { ?>
 			<td class="linecoluht_currency right"><span id="title_up_ht_currency"><?php echo $langs->trans('PriceUHTCurrency'); ?></span></td>
 		<?php } ?>
-		<?php if (!empty($inputalsopricewithtax) && !getDolGlobalInt('MAIN_NO_INPUT_PRICE_WITH_TAX')) { ?>
+		<?php /*if (!empty($inputalsopricewithtax) && !getDolGlobalInt('MAIN_NO_INPUT_PRICE_WITH_TAX')) { ?>
 			<td class="linecoluttc right"><span id="title_up_ttc"><?php echo $langs->trans('PriceUTTC'); ?></span></td>
-		<?php } ?>
+		<?php }*/ ?>
 		<td class="linecolqty right"><?php echo $langs->trans('Qty'); ?></td>
 		<?php
 		if (getDolGlobalInt('PRODUCT_USE_UNITS')) {
