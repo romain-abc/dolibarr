554b87581f2a4f4df9c7e64b5850d67d8869d495
romain-abc <romain@abc-com.fr>
2026-04-15 16:46:40 +0200

    bugfixes

--- htdocs/fourn/class/fournisseur.class.php
+++ htdocs/fourn/class/fournisseur.class.php
@@ -68,11 +68,12 @@ class Fournisseur extends Societe
 	 *    @param    string	$ref_alias 		Name_alias of third party (Warning, this can return several records)
 	 * 	  @param	int		$is_client		Is the thirdparty a client ?
 	 *    @param	int		$is_supplier	Is the thirdparty a supplier ?
+	 *    @param    string  $code_client    Code_client of third party (Warning, this can return several records)
 	 *    @return   int						>0 if OK, <0 if KO or if two records found for same ref or idprof, 0 if not found.
 	 */
-	public function fetch($rowid, $ref = '', $ref_ext = '', $barcode = '', $idprof1 = '', $idprof2 = '', $idprof3 = '', $idprof4 = '', $idprof5 = '', $idprof6 = '', $email = '', $ref_alias = '', $is_client = 0, $is_supplier = 1)
+	public function fetch($rowid, $ref = '', $ref_ext = '', $barcode = '', $idprof1 = '', $idprof2 = '', $idprof3 = '', $idprof4 = '', $idprof5 = '', $idprof6 = '', $email = '', $ref_alias = '', $is_client = 0, $is_supplier = 1, $code_client = '')
 	{
-		return parent::fetch($rowid, $ref, $ref_ext, $barcode, $idprof1, $idprof2, $idprof3, $idprof4, $idprof5, $idprof6, $email, $ref_alias, $is_client, $is_supplier);
+		return parent::fetch($rowid, $ref, $ref_ext, $barcode, $idprof1, $idprof2, $idprof3, $idprof4, $idprof5, $idprof6, $email, $ref_alias, $is_client, $is_supplier, $code_client);
 	}
 
 	/**
